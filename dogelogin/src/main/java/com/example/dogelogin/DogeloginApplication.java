41c5fa31ae003d6b844d77cacf60aee9968f00c6
KimHyungDo <71310026+KimHyungDo@users.noreply.github.com>
2021-04-29 00:21:28 +0900

    gg

---
+++ dogelogin/src/main/java/com/example/dogelogin/DogeloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogeloginApplication.class, args);
+	}
+
+}
---
+++ dogelogin/src/test/java/com/example/dogelogin/DogeloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogeloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
