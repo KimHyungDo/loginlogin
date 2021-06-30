f3bfcec7315c40f4b56efb4072ad566ed4b00da2
KimHyungDo <71310026+KimHyungDo@users.noreply.github.com>
2021-06-30 16:14:38 +0900

    gg

--- dogelogin/src/main/java/com/example/dogelogin/repository/UserRepository.java
+++ dogelogin/src/main/java/com/example/dogelogin/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class UserRepository {
@@ -16,5 +17,19 @@ public class UserRepository {
         em.persist(user);
     }
 
+    public User findOne(Long id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select m from User m", User.class)
+                .getResultList();
+    }
+
+    public List<User> findByName(String name) {
+        return em.createQuery("select m from User m where m.name = :name", User.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
 
 }
---
+++ dogelogin/src/main/java/com/example/dogelogin/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.dogelogin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
